3d260d9b7ed3bd02d5b314458d70c11224c5feac
Javier Laguna <javier.laguna@innocv.com>
2025-07-29 18:09:21 +0200

    Create and implement sqldelight database

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,19 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.sqldelight)
 }
 
 group = "dev.jlaguna"
 version = "0.0.1"
+
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName = "dev.jlaguna.notes.database"
+        }
+    }
+}
 
 application {
     mainClass = "io.ktor.server.netty.EngineMain"
@@ -25,6 +34,7 @@ dependencies {
     implementation(libs.ktor.server.html.builder)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.sqlite.driver)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- src/main/kotlin/repositories/NotesRepository.kt
+++ src/main/kotlin/repositories/NotesRepository.kt
@@ -1,29 +1,42 @@
 package dev.jlaguna.repositories
 
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import dev.jlaguna.models.Note
+import dev.jlaguna.notes.database.AppDatabase
+import dev.jlaguna.notes.database.DbNote
+import java.io.File
+
+private const val DATABASE_NAME = "database.db"
 
 object NotesRepository {
 
-    private val list = mutableListOf<Note>()
-    private var currentId = 1L
+    private val notesDb = JdbcSqliteDriver(url = "jdbc:sqlite:$DATABASE_NAME").let {
+        if (!File(DATABASE_NAME).exists()) {
+            AppDatabase.Schema.create(it)
+        }
+        AppDatabase(it)
+    }.noteQueries
 
-    fun save(note: Note): Note =
-        note.copy(id = currentId++)
-            .also(list::add)
+    fun save(note: Note): Note {
+        notesDb.insert(note.title, note.description, note.type.name)
+        return notesDb.selectLastInsertedNote().executeAsOne().toNote()
+    }
 
-    fun getAll(): List<Note> = list
+    fun getAll(): List<Note> = notesDb.select().executeAsList().map { it.toNote() }
 
-    fun getById(id: Long): Note? = list.find { it.id == id }
+    fun getById(id: Long): Note? = notesDb.selectById(id).executeAsOneOrNull()?.toNote()
 
-    fun update(note: Note): Boolean  =
-        list.indexOfFirst { it.id == note.id }
-            .takeIf { it >= 0 }
-            ?.also { list[it] = note }
-            .let { it != null }
+    fun update(note: Note): Boolean {
+        if (getById(note.id) == null) return false
+        notesDb.update(note.title, note.description, note.type.name, note.id)
+        return true
+    }
 
-    fun delete(id: Long): Boolean =
-        list.indexOfFirst { it.id == id }
-            .takeIf { it >= 0 }
-            ?.also(list::removeAt)
-            .let { it != null }
+    fun delete(id: Long): Boolean {
+        if (getById(id) == null) return false
+        notesDb.delete(id)
+        return true
+    }
 }
+
+private fun DbNote.toNote() = Note(id, title, description, Note.Type.valueOf(type))
